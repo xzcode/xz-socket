caada516899529ceb1ca9d771c4449f42ad36201
xiezai <379551875@qq.com>
2018-10-03 11:50:21 +0800

    2018-10-03 11:50:20

--- xz-socket-test-server/src/main/java/com/xzcode/socket/test/server/config/SpringConfig.java
+++ xz-socket-test-server/src/main/java/com/xzcode/socket/test/server/config/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.xzcode.socket.test.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,7 +9,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class SpringConfig {
 	
-	
+	@Bean
 	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(connectionFactory);
